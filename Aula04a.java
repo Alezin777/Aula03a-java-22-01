0e10d2e1e51fdf45c2f216571bdb45090030144c
Alezin777 <160975381+Alezin777@users.noreply.github.com>
2025-01-23 20:07:44 -0300

    Add files via upload

---
+++ Aula04a.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class ClasseMain {
+  
+  public static void main(String[] args) {
+    
+    int senhaDigitada = 8888;
+    
+    if (validaSenha(senhaDigitada)){
+      System.out.println("Permitido");
+    } else{
+      System.out.println("Negado");
+    }
+  }
+  
+  public static boolean validaSenha(int minhaSenha){
+    
+    return minhaSenha == 1234;
+
+  }
