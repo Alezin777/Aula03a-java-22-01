5baa15198137a9305c833831ae4b75256add804a
Alezin777 <160975381+Alezin777@users.noreply.github.com>
2025-01-23 21:48:02 -0300

    Add files via upload

---
+++ Aula04C.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ClasseMain {
+  public static void main(String[] args) {
+    /*Aqui eu ANINHEIO  o codigo*/
+    
+    int nrDoPedido = 3;
+    
+    if (nrDoPedido == 1){
+    System.out.println("Big-Mac");
+      }else{
+      if(nrDoPedido == 2){
+      System.out.println("MC. Chicken");
+        }else{
+          if(nrDoPedido == 3){
+          System.out.println("MC. Feliz");
+        }
+      }  
+    }
+  }
+}
+  
