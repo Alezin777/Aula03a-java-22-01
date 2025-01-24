cccb387eae6c7ddf9bba4c1605fb9199e8fb2907
Alezin777 <160975381+Alezin777@users.noreply.github.com>
2025-01-23 21:10:53 -0300

    Add files via upload

---
+++ Auva04B.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class ClasseMain {
+  public static void main(String[] args) {
+    
+    
+    int nr1DaSemana = 5;
+    
+    if (nr1DaSemana == 1){
+    System.out.println("Domingo");
+    }
+    
+    if(nr1DaSemana == 2){
+    System.out.println("Segunda-Feira");
+    }
+    
+    if(nr1DaSemana == 3){
+    System.out.println("Terça-Feira");
+    }
+    
+    if(nr1DaSemana == 4){
+    System.out.println("Quarta-Feira");
+    }
+    
+    if(nr1DaSemana == 5){
+    System.out.println("Quinta-Feira");
+    }
+    
+    if(nr1DaSemana == 6){
+    System.out.println("Sexta-Feira");
+    }
+    
+    if(nr1DaSemana == 7){
+    System.out.println("Sabado");
+    }
+    
+    if(nr1DaSemana > 7){
+    System.out.println("Numero invalido");
+    }
+  
+  }
+}
+  
