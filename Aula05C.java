661603d45bb3174b592a5ca70302e2ad8e6eda2e
Alezin777 <160975381+Alezin777@users.noreply.github.com>
2025-01-24 20:23:48 -0300

    Add files via upload

---
+++ Aula05C.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      //nunca colocar 0 no "cont + 0" senao o pc explode 
+      for (int cont = 0; cont < 51; cont = cont + 1 ){
+        
+        System.out.println("Bittencort");  
+      }
+
+
+
+
+  }
+}
