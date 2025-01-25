bc7a8803de22b1d24dd99d5d2295cec711248af9
Alezin777 <160975381+Alezin777@users.noreply.github.com>
2025-01-24 21:18:30 -0300

    Add files via upload

---
+++ Aula05D.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      Scanner scanner = new Scanner(System.in);
+    
+      System.out.println("Digite a tabuada desejada: ");
+      int tabuada = scanner.nextInt();
+      
+      int cont = 0;
+      
+      while (cont <= 10){
+        System.out.println( tabuada + " x " + cont + " = " + (cont * tabuada ));
+        cont = cont + 1; 
+      }
+
+
+
+
+  }
+}
