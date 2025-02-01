6379f964e15a560b330aa81aafb945f8c52a66b0
Alessandro <160975381+Alezin777@users.noreply.github.com>
2025-01-31 21:45:11 -0300

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+		
+		//String[] flores = new String[10];
+		
+		ArrayList<String> flores = new ArrayList<>();
+
+		//flores[0] = "Margarida";
+		
+		flores.add("Marcarida");
+		flores.add("Violeta");
+		flores.add("Orquidea");
+		flores.add("Lirius");
+		flores.add("Girasol");
+		flores.add("Gravo");
+		flores.add("Cristamo");
+		flores.add("Jasmine");
+		
+		
+		// primeiro eu coloco uma memoria(flor) e depois a lista(flores)
+		for (String flor :  flores) {
+			System.out.println(flor);
+			
+		}
+		
+		
+	}
+
+}
